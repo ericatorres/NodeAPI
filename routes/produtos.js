23f5c3c45cd88549e0f5d42b59afa69b55664fb8
erica.torres <ericavaleska@gmail.com>
2022-12-06 22:02:24 -0300

    rotas de pedidos e de produtos

---
+++ routes/produtos.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Todos os produtos'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Produto inserido'
+    });
+});
+
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    
+    if(id === 'especial'){
+        res.status(200).send({
+            mensagem: 'Você usou o ID especial',
+            id: id
+        });
+    }else {
+        res.status(200).send({
+            mensagem: 'Você usou um ID',
+            id: id
+        });
+    }
+});
+
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Produto alterado'
+    });
+});
+
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Produto excluído'
+    });
+});
+
+module.exports = router;
