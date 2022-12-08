c5a146157fba344cf2fcb1b11fff76846eff6358
erica.torres <ericavaleska@gmail.com>
2022-12-07 21:05:16 -0300

    primeiros tratamentos de erros

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const rotaPedidos = require('./routes/pedidos');
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
-app.use('/teste',(req, res, next) => {
-    res.status(200).send({ 
-        mensagem: 'Ok, funcionou!!'
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     });
 });
 
